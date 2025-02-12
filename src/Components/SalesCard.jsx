5d9069008309802cf96763ceea8f568e7053f767
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-02-12 18:02:27 +0530

    fixed navbar

--- src/Components/SalesCard.jsx
+++ src/Components/SalesCard.jsx
@@ -1,7 +1,9 @@
 import '../styles/SalesCard.css'
+import { useContext } from 'react';
+import { ThemeContext } from '../App';
 
 export default function SalesCard() {
-
+    const { theme, toggleTheme } = useContext(ThemeContext);
     let items = [ // Renamed to 'items' for clarity
         { itemImage: "Images/laptop1.png", itemName: "Laptop of India by amazon", itemAbt: "This is the best waterproof laptop....", itemPrice: "Rs.49999" },
         { itemImage: "Images/smartphone1.png", itemName: "Smartphone of India by amazon", itemAbt: "This is the best waterproof laptop....", itemPrice: "Rs.7999" },
@@ -15,7 +17,7 @@ export default function SalesCard() {
         <>
             {
                 items.map((product, index) => ( // Correct usage: product, index
-                    <div className="sales-card" key={index}> {/* Added key prop */}
+                    <div className={`sales-card ${theme}`} key={index}> {/* Added key prop */}
                         <div className="item-img">
                             <img src={product.itemImage} alt="Images" /> {/* Use product.itemImage */}
                             <div className="add-to-cart">
--- src/Pages/TopListed.jsx
+++ src/Pages/TopListed.jsx
@@ -1,10 +1,13 @@
 import SalesCard from "../Components/SalesCard";
 import '../styles/TopListed.css'
+import { useContext } from 'react';
+import { ThemeContext } from '../App';
 
 export default function TopListed() {
+    const { theme, toggleTheme } = useContext(ThemeContext);
     return (
         <>
-            <div className="top-listed">
+            <div className={`top-listed ${theme}`}>
                 <h1>Best Sales Item</h1>
                 <div className="sales-cards">
                     <SalesCard />
