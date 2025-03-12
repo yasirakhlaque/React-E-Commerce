3c66ba62d4c15da87ce54ebb4a4da545739b9720
Yasir Akhlaque <akhlaqueyasir28@gmail.com>
2025-03-12 16:26:13 +0530

    Added animation to toaster

--- src/Components/SuccessAdd.jsx
+++ src/Components/SuccessAdd.jsx
@@ -1,7 +1,9 @@
+import '../styles/SuccessAdd.css'
+
 export default function SuccessAdd({state}) {
     return (
-        <div style={{ position: "fixed", bottom: "10px", right: "10px", background: "#2424428a", color: "white", padding: "10px", borderRadius: "5px",border:"1px solid grey" }}>
-            Item {state} Cart!
+        <div style={{ position: "fixed", bottom: "10px", right: "10px", background: "#2424428a", color: "white", padding: "10px", borderRadius: "5px",border:"1px solid grey" }} className='Success-Add'>
+         <i className="fa-solid fa-circle-question"></i>   Item {state} Cart!
         </div>
     );
 }
